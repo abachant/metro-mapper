9d6c9aec6d8de0232f37f4eac1c8bed636000e5b
abachant <aubreybachant@gmail.com>
2021-03-31 22:55:03 -0400

    Update rendering of route to show each stop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Polyline, CircleMarker } from 'react-leaflet';
 
 import System from './System.jsx';
 import Map from './Map.jsx';
@@ -13,31 +13,42 @@ const App = () => {
     setStops([...stops, stop]);
   };
 
-  const renderMarker = () => {
-    if (newStop.length > 1) {
+  const renderNewStop = () => {
+    if (newStop.length > 0) {
       return (
         <Marker position={newStop}>
           <Popup>
-            <StopForm addStop={addStop} newStop={newStop} setNewStop={setNewStop}/>
+            <StopForm addStop={addStop} newStop={newStop} setNewStop={setNewStop} />
           </Popup>
         </Marker>
       );
     }
   };
 
-  // const
+  const renderStops = () => {
+    return stops.map((stop) => {
+      return (
+        <CircleMarker pathOptions={{ color: 'red', fillColor: 'white', fillOpacity: 1 }} radius="5" center={stop.latlng}>
+          <Popup>
+            <div>{stop.name}</div>
+          </Popup>
+        </CircleMarker>
+      )
+    })
+  };
 
   return (
     <div>
       <h1>Metro Mapper</h1>
-      <MapContainer style={{ height: '500px' }} center={[51.505, -0.09]} zoom={13}>
+      <MapContainer style={{ height: '550px' }} center={[42.359994, -71.056562]} zoom={13}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Map setNewStop={setNewStop} />
-        <Polyline pathOptions={{ color: 'red' }} positions={stops.map(stop=>stop.latlng)} />
-        {renderMarker()}
+        <Polyline pathOptions={{ color: 'red' }} positions={stops.map(stop => stop.latlng)} />
+        {renderNewStop()}
+        {renderStops()}
       </MapContainer>
       {/* <System stops={stops} addStop={addStop} /> */}
     </div>
