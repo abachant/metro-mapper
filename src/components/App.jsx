c012963aca861d1dda90487fb66c67c220ecda73
abachant <aubreybachant@gmail.com>
2021-03-31 21:43:41 -0400

    Update leaflet map to add a polyline for clicked locations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, Polyline} from 'react-leaflet';
+
 import System from './System.jsx';
 import Map from './Map.jsx';
 
 const App = () => {
-  const [stops, setStops] = useState(['Fanieul Square', 'Haymarket Square']);
+  const [stops, setStops] = useState([]);
+  // const [newStop, setNewStop] = useState();
   // add new stop to end of the line
   const addStop = (newStop) => {
     setStops([...stops, newStop]);
   };
 
+  // const renderMarkers
+
   return (
     <div>
       <h1>Metro Mapper</h1>
-      <Map />
+      <MapContainer style={{ height: '500px' }} center={[51.505, -0.09]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Map addStop={addStop}/>
+        <Polyline pathOptions={{color: 'red'}} positions={stops} />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+        </Marker>
+      </MapContainer>
       <System stops={stops} addStop={addStop} />
     </div>
   );
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,42 +1,19 @@
-import React, { useEffect } from 'react';
-import ReactDOMServer from 'react-dom/server';
+import React, { useState } from 'react';
+// import ReactDOMServer from 'react-dom/server';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet';
 import L from 'leaflet';
 import StopForm from './StopForm.jsx';
-import { accessKey } from '../../accessToken.json';
+// import { accessKey } from '../../accessToken.json';
 
-const Map = () => {
-  useEffect(() => {
-    const primaryMap = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox/streets-v11',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: accessKey,
-    }).addTo(primaryMap);
-
-    const popup = L.popup();
-
-    const onMapClick = (e) => {
-      // console.log(e.latlng)
-      popup
-        .setLatLng(e.latlng)
-        // "You clicked the map at " + e.latlng.toString()
-        // .setContent(ReactDOMServer.renderToString(<StopForm />))
-        .setContent('<div>you just clicked</div>')
-        .openOn(primaryMap);
-
-      const marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(primaryMap);
-      marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
-    };
-
-    primaryMap.on('click', onMapClick);
-  }, []);
-
-  return (
-    <div id="map" style={{ height: '500px' }} />
-  );
+const Map = ({addStop}) => {
+  // const [newStop, setNewStop] = useState();
+  const map = useMapEvents({
+    click: (e) => {
+      e.originalEvent.preventDefault();
+      addStop([e.latlng.lat, e.latlng.lng]);
+    },
+  });
+  return null;
 };
 
 export default Map;
